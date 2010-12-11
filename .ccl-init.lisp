4c791344a5f5490c6a663c0f91f334555b58d872
Brit Butler <redline6561@gmail.com>
2010-12-11 15:46:39 -0500

    Small stump and lisp-init fixups.

--- .ccl-init.lisp
+++ .ccl-init.lisp
@@ -1,15 +1,13 @@
 (require 'asdf)
 
 ;; Make it so we can load via ASDF just by using REQUIRE (I'm lazy)
-;; mostly stolen from http://www.mail-archive.com/cmucl-help-bounce@cons.org/msg02514.html
+;; stolen from http://www.mail-archive.com/cmucl-help-bounce@cons.org/msg02514.html
 (defun module-provide-asdf (name)
   (let* ((name (string-downcase name))
      (system (asdf:find-system name nil)))
     (when system
       (asdf:oos 'asdf:load-op name)
       t)))
-
-(setf asdf:*central-registry* '("/home/redline/builds/clbuild-dev/systems/" *default-pathname-defaults*))
 
 ;; hook it in
 (pushnew 'module-provide-asdf *module-provider-functions*)
@@ -22,3 +20,5 @@
 (let ((quicklisp-init #p"/media/redlinux/home/redline/Desktop/quicklisp/setup.lisp"))
   (when (probe-file quicklisp-init)
     (load quicklisp-init)))
+
+(push "/home/redline/projects/systems/" asdf:*central-registry*)
